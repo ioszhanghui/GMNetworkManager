c0eb6b72adccff84911beaa400049372e2314731
dorayo <yomingyo@gmail.com>
2019-10-14 10:43:02 +0800

    Initial commit

---
+++ Example/GMNetworkManager/GMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GMAppDelegate.h
+//  GMNetworkManager
+//
+//  Created by ioszhanghui@163.com on 10/14/2019.
+//  Copyright (c) 2019 ioszhanghui@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
